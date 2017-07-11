14da5f5095060494f38da5dd0676b318dfed4584
LebedenkoNN <totx@narod.ru>
2017-07-11 19:51:32 +0300

    Added ReturnValueOfFunctionMustBeOfTheTypeName.php to error`s collection

---
+++ src/ErrorsCollection/FatalError/Uncaught/TypeError/ReturnValueOfFunctionMustBeOfTheTypeName.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+function getInt() :int
+{
+    return '1';// PHP Fatal error:  Uncaught TypeError: Return value of z() must be of the type integer, string returned in
+}
+getInt();
